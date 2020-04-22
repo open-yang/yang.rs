2b1d8a484bc746b7c2780bff89fa5aed99c32bb5
武安祥 <wuanxiang@qianxin.com>
2020-04-22 09:57:24 +0800

    :sparkles: feat(0.1.0): 初始化clap
    Signed-off-by: 武安祥 <wuanxiang@qianxin.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use clap::App; 
+
+fn main() { 
+    App::new("yang")
+       .version("1.0")
+       .about("Does great things!")
+       .author("Kevin K.")
+       .get_matches();
 }
