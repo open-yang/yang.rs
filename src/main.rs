dfd623486c3298da44b9fe274d306919967069de
carloswu <carloswu@tencent.com>
2020-09-17 09:51:50 +0800

    merge conflict

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use clap::App; 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
 
-fn main() { 
-    App::new("yang")
-       .version("1.0")
-       .about("Does great things!")
-       .author("Kevin K.")
-       .get_matches();
+fn main() {
+    let matches = App::new("yang")
+                          .version("1.0")
+                          .author("Carlos Wu.")
+                          .about("Does awesome things")
+                          .subcommand(SubCommand::with_name("gen")
+                                      .about("Generating rust source code through Yang files")
+                                      .version("1.0")
+                                      .arg(Arg::with_name("debug")
+                                          .short("d")
+                                          .help("print debug information verbosely")))
+                          .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("gen") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
 }
