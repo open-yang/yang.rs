885aef884b588e8e3e75eee4000e9c525177a0a8
carloswu <carloswu@tencent.com>
2020-09-24 14:17:26 +0800

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::{App, Arg, SubCommand};
 use std::path::Path;
-use yang::cmd::generate::{parse_yang};
+use yang::cmd::generate::parse_yang;
 
 fn main() {
     let matches = App::new("yang")
@@ -30,7 +30,10 @@ fn main() {
         )
         .get_matches();
     if let Some(matches) = matches.subcommand_matches("gen") {
-        println!("Value for language: {}", matches.value_of("language").unwrap());
+        println!(
+            "Value for language: {}",
+            matches.value_of("language").unwrap()
+        );
         println!("Value for dir: {}", matches.value_of("dir").unwrap());
         if let Err(err) = parse_yang(Path::new(matches.value_of("dir").unwrap())) {
             println!("error {}", err);
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,42 +1,40 @@
 extern crate nom;
 
 use nom::{
-  IResult,
-  bytes::complete::{tag, take_while_m_n},
-  combinator::map_res,
-  sequence::tuple};
+    bytes::complete::{tag, take_while_m_n},
+    combinator::map_res,
+    sequence::tuple,
+    IResult,
+};
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Color {
-  pub red:     u8,
-  pub green:   u8,
-  pub blue:    u8,
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
 }
 
 #[allow(dead_code)]
 fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
-  u8::from_str_radix(input, 16)
+    u8::from_str_radix(input, 16)
 }
 
 #[allow(dead_code)]
 fn is_hex_digit(c: char) -> bool {
-  c.is_digit(16)
+    c.is_digit(16)
 }
 
 #[allow(dead_code)]
 fn hex_primary(input: &str) -> IResult<&str, u8> {
-  map_res(
-    take_while_m_n(2, 2, is_hex_digit),
-    from_hex
-  )(input)
+    map_res(take_while_m_n(2, 2, is_hex_digit), from_hex)(input)
 }
 
 #[allow(dead_code)]
 fn hex_color(input: &str) -> IResult<&str, Color> {
-  let (input, _) = tag("#")(input)?;
-  let (input, (red, green, blue)) = tuple((hex_primary, hex_primary, hex_primary))(input)?;
+    let (input, _) = tag("#")(input)?;
+    let (input, (red, green, blue)) = tuple((hex_primary, hex_primary, hex_primary))(input)?;
 
-  Ok((input, Color { red, green, blue }))
+    Ok((input, Color { red, green, blue }))
 }
 
 #[cfg(test)]
@@ -45,10 +43,124 @@ mod tests {
 
     #[test]
     fn it_adds_two() {
-        assert_eq!(hex_color("#2F14DF"), Ok(("", Color {
-            red: 47,
-            green: 20,
-            blue: 223,
-          })));
+        assert_eq!(
+            hex_color("#2F14DF"),
+            Ok((
+                "",
+                Color {
+                    red: 47,
+                    green: 20,
+                    blue: 223,
+                }
+            ))
+        );
+    }
+
+    #[test]
+    fn pares_obj_key() {
+        let obj_key = r#"module svr-obj {
+  yang-version 1;
+  namespace "hyperion:rules:yang:service";
+  prefix "service";
+
+  import obj-key {
+      prefix obj-key;
+      revision-date "2020-01-15";
+  }
+
+  organization "tencent";
+
+  description "ip address object.";
+
+  revision "2020-01-15" {
+      description "Initial revision of ip address object model";
+  }
+  
+  typedef num-set {
+      type union {
+          type uint32;
+          type string;
+      }
+  }
+
+  grouping port-pair {
+      leaf-list source {
+          type num-set;
+      }
+      leaf-list dest {
+          type num-set;
+      }
+  }
+
+  grouping control-messages {
+      leaf typ {
+          type uint8;
+      }
+      leaf code {
+          type num-set;
+      }
+  }
+
+  grouping svr-obj {
+      uses obj-key:obj-key;
+      list protocols {
+          choice protocol {
+              case ip {
+                  leaf number {
+                      type uint32;
+                  }
+              }
+              case tcp {
+                  container tcp {
+                      uses port-pair;
+                  }
+              }
+              case udp {
+                  container udp {
+                      uses port-pair;
+                  }
+              }
+              case icmp {
+                  container icmp {
+                      uses control-messages;
+                  }
+                  
+              }
+              case icmpv6 {
+                  container icmpv6 {
+                      uses control-messages;
+                  }
+              }
+          }
+      }
+      list children {
+          uses obj-key:obj-key;
+      }
+  }
+
+    grouping svr-objs {
+      list svr-objs {
+          uses svr-obj;
+      }
+  }
+
+  container svr-obj-container {
+      uses svr-objs;
+  }
+
+  rpc get {
+      output {
+          uses svr-objs;
+      }
+  }
+
+  rpc edit {
+      input {
+          uses svr-objs;
+      }
+  }
+}      
+        "#;
+        println!("{}", obj_key)
     }
 }
